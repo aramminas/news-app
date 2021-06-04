270ecee22cc5a36c06ce61e48be4bef6cdbb2920
VahanGev <47473115+VahanGev@users.noreply.github.com>
2021-06-04 14:42:55 +0400

    Added noMoreSources Component

---
+++ src/components/other/noMoreSources/NoMoreSources.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './NoMoreSources.scss';
+function NoMoreSources() {
+    return (
+        <div className="no-more-source">
+            <span>All Sources Are Loaded</span>
+        </div>
+    )
+}
+
+export default NoMoreSources
